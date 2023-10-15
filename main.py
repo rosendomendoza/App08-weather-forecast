cdc279684d923ffb96edeea033afd6aa5d96f39e
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-15 20:37:46 +0200

    Day 32: Add Widgets

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 
+st.set_page_config(layout="centered")
+
+st.title("Weather Forecast for the next days ")
+
+place = st.text_input(label="Place:", placeholder="Enter a place")
+
+days = st.slider(label="Forecast Days:", min_value=1, max_value=5, help="Select the number of forecasted days" )
+
+type_data = st.selectbox(label="Select data to view: ",
+                         options=["Temperature", "Sky"])
+
+st.subheader(f"{type_data} for the {days} next day(s) in {place}")
